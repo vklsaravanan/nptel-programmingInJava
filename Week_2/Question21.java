871ee3a0535fcdf79a3425cada0bbc45eab19e1d
SARAVANAN RAJA <70447974+vklsaravanan@users.noreply.github.com>
2021-08-12 18:05:21 +0530

    Exercise 1 and 2

---
+++ Week_2/Question21.java
@@ -0,0 +1,51 @@
+Week 2 : Programming Assignment 1
+
+QN : Complete the code segment to call the method  print() of class Student first and then call print() method of class School.
+нн
+...............................................................................
+
+// This is the class named School
+class School { 
+    // This is a method in class School
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+} 
+// This is the class named Student
+class Student { 
+	// This is a method in class Student
+    public void print() { 
+		System.out.println("Hi! I am class STUDENT"); 
+    } 
+}
+public class Question21{ 
+    public static void main(String args[]){   
+    
+        
+// Answer is given below for NPTEL
+...............................................................................
+
+// Creating an object of class Student
+	Student objStudent = new Student();
+
+// Calling 'print()' method of class Student 
+	objStudent.print();
+
+// Creating an object of class School
+	School objSchool = new School();
+
+// Calling 'print()' method of class School
+	objSchool.print();
+	
+...................................................
+NOTE : This will show some presentation error.
+	  But you will get passed status.
+	  Reason : error occurred in main method when 		  	 	  printing ,thats can't able to change in this code, 
+	  If we have any possiblities to fix this error we can 	  	  update this specific code file
+...............................................................................
+
+
+ }
+}
+
+
---
+++ Week_2/Question22.java
@@ -0,0 +1,42 @@
+Week 2 : Programming Assignment 2
+
+QN : Complete the code segment to call the method  print() of class given class Printer to print the following.
+нн
+...............................................................................
+
+// This is the class named Printer
+class Printer { 
+    // This are the methods in class Printer
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+    public void print(String s) { 
+		System.out.println(s); 
+    } 
+} 
+public class Question22{ 
+    public static void main(String[] args) {  
+  
+        
+// Answer is given below for NPTEL
+...............................................................................
+
+// Creating an object of class Printer
+Printer classPrinter =new Printer();
+
+// Calling 'print()' methods for desired output
+classPrinter.print("Hi! I am class STUDENT");
+classPrinter.print();
+
+...................................................
+NOTE : This will show some presentation error.
+	  But you will get passed status.
+	  Reason : error occurred in main method when 		  	 	  printing ,thats can't able to change in this code, 
+	  If we have any possiblities to fix this error we can 	  	  update this specific code file
+
+
+...............................................................................
+
+
+ }
+}
