ff7bc8e2c9c657007020c4059d8fa64481d6722d
SARAVANAN R <70447974+vklsaravanan@users.noreply.github.com>
2021-08-27 20:18:53 +0530

    Week_3 assignment 1

--- Week_3/Fibonacci.java
+++ Week_3/Fibonacci.java
@@ -1,7 +1,7 @@
 // Week 3: Programming Assignment 1
 
 /*QN : This program is related to the generation of Fibonacci numbers.>
-   For example: 0,1, 1,2, 3,5, 8, 13,… is a Fibonacci sequence where 13 is the 8th Fibonacci number.*/
+   For example: 0,1, 1,2, 3,5, 8, 13,â€¦ is a Fibonacci sequence where 13 is the 8th Fibonacci number.*/
 //..........................................................................
     
 import java.util.Scanner; //This package for reading input
