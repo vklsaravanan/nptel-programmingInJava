6f945943b5c974b9165447dccc52cfdfc4b83329
SARAVANAN R <70447974+vklsaravanan@users.noreply.github.com>
2021-08-27 19:40:27 +0530

    week_4 assignment 1

---
+++ Week_4/Question41.java
@@ -0,0 +1,25 @@
+//Week 4 : Programming Assignment 1
+
+/*QN :Complete the code segment to execute the following program successfully. 
+      You should import the correct package(s) and/or class(s) to complete the code.
+
+*Answer is given below for --+ NPTEL +--
+...............................................................................*/
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+//...............................................................................
+
+//main class Question is created
+public class Question41{
+public static void main(String[] args) {
+	// Scanner object is created
+ Scanner scanner = new Scanner(System.in);
+  // Read String input using scanner class
+ String courseName = scanner.nextLine(); 
+  // Print the scanned String
+ out.println("Course: " + courseName); 
+}
+}
+
