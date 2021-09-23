32e7735e68f22994008960d9e132712741920e28
SARAVANAN R <70447974+vklsaravanan@users.noreply.github.com>
2021-09-23 19:53:34 +0530

    week_8 assignment 1

---
+++ Week_8/Pattern1.java
@@ -0,0 +1,34 @@
+/* Week 8 : Programming Assignment 1
+QN : Write a program which will print a pyramid of "*" 's of height "n" and print the number of "*" 's in the pyramid.
+     For example:
+     Input : 5
+Output:  
+
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+25
+    Answer is given below for --+ NPTEL +--
+...............................................................................*/
+import java.util.*;
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner inr = new Scanner(System.in);
+	   int n = inr.nextInt();
+//____________________________________________________
+      
+      for(int i=1; i<=n; i++) {
+       int k=0;
+        for(int j=1; j<=(n-i); j++) {
+            System.out.print("  ");
+        }
+        while(k != (2*i-1)) {
+            System.out.print("* ");
+            k++;
+        }
+        System.out.println();}
+    System.out.println(n*n);
+    }
+}
