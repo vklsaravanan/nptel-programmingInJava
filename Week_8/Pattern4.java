7f351175da1a8ac4b5d721ce5c78a22ccbe1ec8c
SARAVANAN R <70447974+vklsaravanan@users.noreply.github.com>
2021-09-24 22:06:13 +0530

    week_8 assignment 4

---
+++ Week_8/Pattern4.java
@@ -0,0 +1,58 @@
+/* Week 8 : Programming Assignment 4
+
+QN : Write a program to print symmetric Pascal's triangle of "*" 's of  height "l" of odd length . If input "l" is even then your program will print "Invalid line number".
+For example:
+input : 5
+output:
+  *
+ * *
+* * *
+ * *
+  *
+input : 6
+output:
+Invalid line number
+
+     Answer is given below for --+ NPTEL +--
+...............................................................................*/
+
+import java.util.*;
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner inr = new Scanner(System.in);
+	int n = inr.nextInt();
+      int k ;
+        // Add the necessary code in the below space
+      if(n % 2 == 0)
+        System.out.print("Invalid line number");
+      else{
+        for(int i = 0; i < n; i++){
+          k = 0;
+          if(i <= n/2){
+          for(int j = n - i; j > n/2 + 1;j--){
+            System.out.print(" ");
+          }
+            while(k < ( i + 1 )){
+              System.out.print("*" + " ");
+            k++;
+            }
+          }
+          else{
+            for(int j = n-i; j < n/2 + 1;j++)
+              System.out.print(" ");
+            while(k < (n- i  )){
+              if(k == n-i-1)
+                System.out.print("*");
+              else{
+              System.out.print("*");
+              System.out.print(" ");
+              }
+            k++;
+            }
+          }
+          if(i != n-1)
+           System.out.println( );
+        }
+      }
+    }
+}
