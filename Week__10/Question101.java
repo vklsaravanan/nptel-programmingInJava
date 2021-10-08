5dadea4e3c0bc7a947138dd861ceb5620829a056
SARAVANAN R <70447974+vklsaravanan@users.noreply.github.com>
2021-10-08 20:32:52 +0530

    Update Question101.java

--- Week__10/Question101.java
+++ Week__10/Question101.java
@@ -3,9 +3,14 @@
 QN : The following code needs some package to work properly. Write appropriate code 
     to import the required package(s) in order to make the program compile and execute successfully.
     
+              
+            Answer is given below for --+ NPTEL +--
 ...............................................................................*/
 // Import required packages
+
 import java.sql.*;
+//...............................................................................
+
 public class Question101 {
     public static void main(String args[]) {
         try {
@@ -14,15 +19,14 @@ public class Question101 {
               String DB_URL = "jdbc:sqlite:/tempfs/db";
               System.setProperty("org.sqlite.tmpdir", "/tempfs");
               
-/*          Answer is given below for --+ NPTEL +--
-...............................................................................*/
-          
-           conn = DriverManager.getConnection(DB_URL);
+              // JDBC Codes in the hidden section
+
+              // Open a connection
+              conn = DriverManager.getConnection(DB_URL);
               System.out.print(conn.isValid(1));
               conn.close();
-          
-//...............................................................................
-          // JDBC Codes in the visible section
+
+// JDBC Codes in the visible section
 
         }
        catch(Exception e){ System.out.println(e);}  
